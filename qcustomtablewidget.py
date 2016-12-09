6a9aab9ddf659e0051f1d8c807f01ba0a01c4cfb
Darius Berghe <darius.berghe@europe.tel.com>
2016-12-09 16:07:48 +0100

    Fixed table widget stretch issue on windows

--- qcustomtablewidget.py
+++ qcustomtablewidget.py
@@ -3,23 +3,28 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 class QCustomTableWidget(QtWidgets.QTableWidget):
     def __init__(self, parent):
         super().__init__()
-        # stretch horizontal header's last item to fill the space
-        self.horizontalHeader().setStretchLastSection(True)
         # make the columns proportional
-        self.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        [self.horizontalHeader(i).setSectionResizeMode(QtWidgets.QHeaderView.Stretch) \
+            for i in range(self.columnCount())]
 
     def keyPressEvent(self, QKeyEvent):
         # delete selected items when pressing the keyboard's delete key
         if QKeyEvent.key() == QtCore.Qt.Key_Delete:
             selected = self.selectedItems()
             for i in selected:
-                self.removeRow(i.row())
+                i.setText('')
         QtWidgets.QTableWidget.keyPressEvent(self, QKeyEvent)
+
+    def isLastRowEmpty(self):
+        for i in range(self.columnCount()):
+            if self.model().index(self.rowCount() - 1, i).data() != None:
+                return False
+        return True
 
     def mousePressEvent(self, event):
         # overwrite the mouse press event so that it inserts a new row when clicking on the empty space
         # (provided there is no empty last row already)
-        if self.itemAt(event.pos()) is None and (self.model().index(self.rowCount() - 1, 0).data() != None):
+        if self.itemAt(event.pos()) is None and not self.isLastRowEmpty():
             self.clearSelection()
             self.insertRow(self.rowCount())
         QtWidgets.QTableWidget.mousePressEvent(self, event)
